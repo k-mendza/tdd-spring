42e9e2d8c58b5e235703e4c26a158af407027874
Karol Mendza <karolmendza@gmail.com>
2019-08-25 19:37:27 +0200

    Added Inner class configuration

--- src/test/java/org/springframework/samples/petclinic/exmpl/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/exmpl/HearingInterpreterTest.java
@@ -2,18 +2,29 @@ package org.springframework.samples.petclinic.exmpl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
+@SpringJUnitConfig(classes = HearingInterpreterTest.TestConfig.class)
 class HearingInterpreterTest {
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        HearingInterpreter hearingInterpreter() {
+            return new HearingInterpreter(new LaurelWordProducer());
+        }
+    }
 
     @Autowired
     HearingInterpreter hearingInterpreter;
 
     @Test
-    void whatIheard() {
+    void whatIHeard() {
         String word = hearingInterpreter.whatIHeard();
 
         assertEquals("Laurel", word);
